018e6f41fc3deafeca7c29a4fdb14016e400f423
MarkPolyakov <markpolya@icloud.com>
2024-12-14 20:31:15 +0500

    пофиксил конфликты между проектами

--- insurance_organisation/Program/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ insurance_organisation/Program/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c515ac69c54a07b2eaa623ed2446705cd8e0a767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e8db37fa7a99175170724cb2fdbe4556163aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
